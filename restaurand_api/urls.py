02334a7c7b39e443d451720655bd34a80452f809
Florent Desjardins <florent.desjardins@gmail.com>
2015-07-20 16:31:18 +0200

    remove home view and add redirection to api/v1

--- restaurand_api/urls.py
+++ restaurand_api/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 from rest_framework import routers
 from restaurants.views import RestaurantViewSet
-from .views import home
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'restaurants', RestaurantViewSet)
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'restaurand_api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', home),
+    url(r'^$', RedirectView.as_view(url='api/v1/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls, namespace='api')),
--- restaurand_api/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
-
-
-def home(request):
-    return HttpResponse('go to /api/v1') 
